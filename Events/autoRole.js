1cc38c2ee1cbad028eea07efee7599467b2e37e2
Antonio Vitor <147101459+antonioxx1@users.noreply.github.com>
2024-03-26 14:00:15 -0300

    Atualizar o autoRole.js

--- Events/autoRole.js
+++ Events/autoRole.js
@@ -4,7 +4,7 @@ const Discord = require('discord.js')
 const client = require('../index')
 
 client.on("guildMemberAdd", (member) => {
-    let cargo_autorole = member.guild.roles.cache.get("1174045753052762223") // Coloque o ID do cargo
+    let cargo_autorole = member.guild.roles.cache.get("") // Coloque o ID do cargo
     if (!cargo_autorole) return console.log("❌ O AUTOROLE não está configurado.")
   
     member.roles.add(cargo_autorole.id).catch(err => {
